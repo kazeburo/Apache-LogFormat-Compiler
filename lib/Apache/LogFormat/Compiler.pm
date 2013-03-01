fdbae30f2778dfaf6c893179d9f192b21515e63d
Masahiro Nagano <kazeburo@gmail.com>
2013-03-01 15:00:13 +0900

    initial import

---
+++ lib/Apache/LogFormat/Compiler.pm
@@ -0,0 +1,41 @@
+package Apache::LogFormat::Compiler;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Apache::LogFormat::Compiler - ...
+
+=head1 SYNOPSIS
+
+  use Apache::LogFormat::Compiler;
+
+=head1 DESCRIPTION
+
+Apache::LogFormat::Compiler is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Nagano
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
