d36add32ff0c10bf972d478845c2f7f8c0ab2a42
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-01-11 21:27:24 +0100

    POSIX::setlocale fails on system without locales (Android)

--- lib/Apache/LogFormat/Compiler.pm
+++ lib/Apache/LogFormat/Compiler.pm
@@ -52,10 +52,10 @@ sub _strftime {
         my $tz = _tzoffset($self,@time);
         $fmt =~ s/%z/$tz/g;
     }
-    my $old_locale = POSIX::setlocale(&POSIX::LC_ALL);
-    POSIX::setlocale(&POSIX::LC_ALL, 'C');
+    my $old_locale = eval { POSIX::setlocale(&POSIX::LC_ALL) };
+    eval { POSIX::setlocale(&POSIX::LC_ALL, 'C') };
     my $out = _posix_strftime($fmt, @time);
-    POSIX::setlocale(&POSIX::LC_ALL, $old_locale);
+    eval { POSIX::setlocale(&POSIX::LC_ALL, $old_locale) };
     return $out;
 };
 
